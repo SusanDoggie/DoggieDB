77a14694a39d58fa3b31bb6eb47224cda0d24206
Susan Cheng <susan.doggie@gmail.com>
2021-01-29 23:58:28 +0800

    clean

--- Sources/DoggieDB/Data/DatabaseData/BSON.swift
+++ Sources/DoggieDB/Data/DatabaseData/BSON.swift
@@ -91,8 +91,7 @@ extension BSON {
         case let .string(value): self = .string(value)
         case let .regex(value):
             
-            guard let _regex = try? NSRegularExpression(pattern: value.pattern, options: value.options) else { throw Database.Error.unsupportedType }
-            guard let regex = try? BSONRegularExpression(from: _regex) else { throw Database.Error.unsupportedType }
+            guard let regex = try? BSONRegularExpression(from: value.nsRegex) else { throw Database.Error.unsupportedType }
             self = .regex(regex)
             
         case let .signed(value): self = .int64(value)
