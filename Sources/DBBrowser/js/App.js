f7581dfd0109c07b225dbaddfd0e5b92622a12cd
Susan Cheng <susan.doggie@gmail.com>
2021-03-08 00:10:14 +0800

    clean

--- Sources/DBBrowser/js/App.js
+++ Sources/DBBrowser/js/App.js
@@ -22,7 +22,11 @@ export default class App extends React.Component {
   render() {
     return (
       <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
-        <Drawer.Navigator>
+        <Drawer.Navigator
+          openByDefault
+          drawerType='permanent'
+          overlayColor="transparent"
+        >
           <Drawer.Screen name="Table" component={TableScreen} />
         </Drawer.Navigator>
       </NavigationContainer>
