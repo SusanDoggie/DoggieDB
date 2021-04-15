d7763e8d8a87661460f03f8a3368c525e4f51420
Susan Cheng <susan.doggie@gmail.com>
2021-04-15 16:58:05 +0800

    clean

--- Package.swift
+++ Package.swift
@@ -40,7 +40,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/SusanDoggie/Doggie.git", .branch("main")),
-        .package(url: "https://github.com/SusanDoggie/SwiftJS.git", from: "1.2.1"),
+        .package(url: "https://github.com/SusanDoggie/SwiftJS.git", from: "1.2.2"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/mongodb/swift-bson", from: "3.0.0"),
         .package(url: "https://github.com/mongodb/mongo-swift-driver.git", from: "1.0.0"),
