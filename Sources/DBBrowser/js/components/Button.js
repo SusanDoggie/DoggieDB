562f292ed57efbdf2c1ca92ac3763daab5e40268
Susan Cheng <susan.doggie@gmail.com>
2021-05-24 07:41:27 +0800

    clean

---
+++ Sources/DBBrowser/js/components/Button.js
@@ -0,0 +1,52 @@
+import _ from 'lodash';
+import React from 'react';
+import { Text, Pressable } from 'react-native';
+
+export default class Button extends React.PureComponent {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isHover: false,
+		};
+	}
+
+	render() {
+
+		const {
+			title,
+			style,
+			titleStyle,
+			hoverStyle,
+			titleHoverStyle,
+			onHoverIn,
+			onHoverOut,
+			...props
+		} = this.props;
+
+		const _onHoverIn = onHoverIn ?? (() => this.setState({ isHover: true }));
+		const _onHoverOut = onHoverOut ?? (() => this.setState({ isHover: false }));
+
+		const _hoverStyle = this.state.isHover ? hoverStyle : {};
+		const _titleHoverStyle = this.state.isHover ? titleHoverStyle : {};
+
+		return <Pressable
+		onHoverIn={_onHoverIn}
+		onHoverOut={_onHoverOut}
+		style={{
+			alignItems: 'center',
+			justifyContent: 'center',
+			backgroundColor: this.state.isHover ? '#1691E8' : '#2196F3',
+			...style,
+			..._hoverStyle
+		}} {...props}>
+			<Text style={{
+				color: 'white',
+				...titleStyle,
+				..._titleHoverStyle
+				}}>{title}</Text>
+		</Pressable>;
+	}
+  }
+  
--- Sources/DBBrowser/js/components/RoundButton.js
+++ Sources/DBBrowser/js/components/RoundButton.js
@@ -1,56 +1,26 @@
 import _ from 'lodash';
 import React from 'react';
-import { Text, Pressable } from 'react-native';
+
+import Button from './Button';
 
 export default class RoundButton extends React.PureComponent {
-
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			isHover: false,
-		};
-	}
 
 	render() {
 
 		const {
-			title,
 			style,
-			titleStyle,
-			hoverStyle,
-			titleHoverStyle,
-			onHoverIn,
-			onHoverOut,
 			...props
 		} = this.props;
 
-		const _onHoverIn = onHoverIn ?? (() => this.setState({ isHover: true }));
-		const _onHoverOut = onHoverOut ?? (() => this.setState({ isHover: false }));
-
-		const _hoverStyle = this.state.isHover ? hoverStyle : {};
-		const _titleHoverStyle = this.state.isHover ? titleHoverStyle : {};
-
-		return <Pressable
-		onHoverIn={_onHoverIn}
-		onHoverOut={_onHoverOut}
+		return <Button
 		style={{
 			minWidth: 96,
 			height: 32,
 			borderRadius: 16,
 			overflow: 'hidden',
-			alignItems: 'center',
-			justifyContent: 'center',
-			backgroundColor: this.state.isHover ? '#1691E8' : '#2196F3',
-			...style,
-			..._hoverStyle
-		}} {...props}>
-			<Text style={{
-				color: 'white',
-				...titleStyle,
-				..._titleHoverStyle
-				}}>{title}</Text>
-		</Pressable>;
+			...style
+		}}
+		{...props} />;
 	}
   }
   
