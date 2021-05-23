06ab1a9aa11512981507fdf8cd3f028c1ed470a4
Susan Cheng <susan.doggie@gmail.com>
2021-05-24 07:34:58 +0800

    test

--- Sources/DBBrowser/js/components/RoundButton.js
+++ Sources/DBBrowser/js/components/RoundButton.js
@@ -1,36 +1,56 @@
 import _ from 'lodash';
 import React from 'react';
-import { View, TouchableWithoutFeedback, Text } from 'react-native';
+import { View, TouchableWithoutFeedback, Text, Pressable } from 'react-native';
 
 export default class RoundButton extends React.PureComponent {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isHover: false,
+		};
+	}
 
 	render() {
 
 		const {
 			title,
-			onPress,
 			style,
 			titleStyle,
+			hoverStyle,
+			titleHoverStyle,
+			onHoverIn,
+			onHoverOut,
 			...props
 		} = this.props;
 
-	  return <TouchableWithoutFeedback onPress={onPress}>
-	  	<View style={{
-			  minWidth: 96,
-			  height: 32,
-			  borderRadius: 16,
-			  overflow: 'hidden',
-			  alignItems: 'center',
-			  justifyContent: 'center',
-			  backgroundColor: '#2196F3',
-			  ...style
-			}} {...props}>
-		  <Text style={{
-			  color: 'white',
-			  ...titleStyle
-			}}>{title}</Text>
-		</View>
-	  </TouchableWithoutFeedback>;
+		const _onHoverIn = onHoverIn ?? (() => this.setState({ isHover: true }));
+		const _onHoverOut = onHoverOut ?? (() => this.setState({ isHover: false }));
+
+		const _hoverStyle = this.state.isHover ? hoverStyle : {};
+		const _titleHoverStyle = this.state.isHover ? titleHoverStyle : {};
+
+		return <Pressable
+		onHoverIn={_onHoverIn}
+		onHoverOut={_onHoverOut}
+		style={{
+			minWidth: 96,
+			height: 32,
+			borderRadius: 16,
+			overflow: 'hidden',
+			alignItems: 'center',
+			justifyContent: 'center',
+			backgroundColor: this.state.isHover ? '#1691E8' : '#2196F3',
+			...style,
+			..._hoverStyle
+		}} {...props}>
+			<Text style={{
+				color: 'white',
+				...titleStyle,
+				..._titleHoverStyle
+				}}>{title}</Text>
+		</Pressable>;
 	}
   }
   
