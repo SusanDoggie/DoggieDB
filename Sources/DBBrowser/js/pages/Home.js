1cb348fcf476ac6d5cd97d57f8ba2644cddb6d34
Susan Cheng <susan.doggie@gmail.com>
2021-05-23 19:03:41 +0800

    test

--- Sources/DBBrowser/js/pages/Home.js
+++ Sources/DBBrowser/js/pages/Home.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import React from 'react';
-import { Button, View, TextInput, Text, ScrollView } from 'react-native';
+import { Button, View, TextInput, Text, ScrollView, StyleSheet } from 'react-native';
 import { withRouter } from "react-router";
 import { EJSON } from 'bson';
 
@@ -60,36 +60,59 @@ class Home extends React.Component {
     }
   }
 
-  render_query_page() {
+  renderDashboard() {
     
-    return (
-      <View style={{ flex: 1, padding: 10 }}>
-        <TextInput
-        multiline
-        onChangeText={(command) => this.setState({ command })}
-        value={this.state.command} />
-        <Button title='Run' onPress={() => this.runCommand()} />
-        <ScrollView style={{ flex: 1 }}>
-        <Text>{this.state.result}</Text>
-        </ScrollView>
-      </View>
-    );
+    return <View style={{ flex: 1 }}>
+      <TextInput
+      multiline
+      onChangeText={(command) => this.setState({ command })}
+      value={this.state.command} />
+      <Button title='Run' onPress={() => this.runCommand()} />
+      <ScrollView style={{ flex: 1 }}>
+      <Text>{this.state.result}</Text>
+      </ScrollView>
+    </View>;
   }
   
-  render_login_page() {
+  renderLoginPanel() {
 
-    return (
-      <View style={{ flex: 1, padding: 10 }}>
-        <TextInput
+    return <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{  }}>
+      <Text>Connection String</Text>
+      <TextInput
+        style={{ 
+          borderBottomWidth: StyleSheet.hairlineWidth, 
+          borderBottomColor: 'black',
+          marginTop: 8,
+        }}
         onChangeText={(connectionStr) => this.setState({ connectionStr })}
         value={this.state.connectionStr} />
-        <Button title='Connect' onPress={() => this.connect()} />
+      <Button
+        style={{
+          marginTop: 8,
+        }}
+        title='Connect'
+        onPress={() => this.connect()} />
       </View>
-    );
+    </View>;
+  }
+
+  renderSideMenu() {
+
+    return <ScrollView>
+    </ScrollView>;
   }
   
   render() {
-    return this.state.isConnected ? this.render_query_page() : this.render_login_page();
+
+    return <View style={{ 
+      flex: 1, 
+      flexDirection: 'row', 
+      alignItems: 'stretch',
+    }}>
+      <View style={{ width: 240, background: 'DarkSlateBlue' }}>{this.renderSideMenu()}</View>
+      <View style={{ flex: 1 }}>{this.state.isConnected ? this.renderDashboard() : this.renderLoginPanel()}</View>
+    </View>;
   }
 }
 
