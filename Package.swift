55792c234989d6cc3c1756d719371e30371a8635
Susan Cheng <susan.doggie@gmail.com>
2021-11-27 00:10:26 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -39,13 +39,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/SusanDoggie/Doggie.git", from: "6.6.1"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
-        .package(url: "https://github.com/mongodb/swift-bson.git", from: "3.0.0"),
-        .package(url: "https://github.com/mongodb/mongo-swift-driver.git", from: "1.1.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.35.0"),
+        .package(url: "https://github.com/mongodb/swift-bson.git", from: "3.0.2"),
+        .package(url: "https://github.com/mongodb/mongo-swift-driver.git", from: "1.2.0"),
         .package(url: "https://gitlab.com/mordil/RediStack.git", from: "1.2.0"),
-        .package(url: "https://github.com/vapor/mysql-nio.git", from: "1.3.0"),
-        .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.6.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.50.0"),
+        .package(url: "https://github.com/vapor/mysql-nio.git", from: "1.3.5"),
+        .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.7.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.53.0"),
     ],
     targets: [
         .target(
