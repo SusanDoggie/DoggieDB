d92a4c53a61f4edf95b289013516c6b896750223
Susan Cheng <susan.doggie@gmail.com>
2021-12-12 19:05:54 +0800

    clean

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ let package = Package(
         .library(name: "DBVapor", targets: ["DBVapor"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/SusanDoggie/Doggie.git", from: "6.6.1"),
+        .package(url: "https://github.com/SusanDoggie/Doggie.git", from: "6.6.2"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.35.0"),
         .package(url: "https://github.com/mongodb/swift-bson.git", from: "3.0.2"),
         .package(url: "https://github.com/mongodb/mongo-swift-driver.git", from: "1.2.0"),
