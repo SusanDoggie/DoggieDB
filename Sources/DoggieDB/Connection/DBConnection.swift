3d51b9cbcaabfdadc083f604da5844a4a4c219b4
Susan Cheng <susan.doggie@gmail.com>
2022-04-22 14:52:24 +0800

    Revert "Update DBConnection.swift"
    This reverts commit 3ab0bdec7b86754853f9a82f31c26cb891279f2c.

--- Sources/DoggieDB/Connection/DBConnection.swift
+++ Sources/DoggieDB/Connection/DBConnection.swift
@@ -73,9 +73,6 @@ extension DBConnection {
             
             do {
                 
-                var options = options
-                options.retryOnConflict = false
-                
                 try await self.withTransaction(options) { connection in
                     
                     for try await element in try await $transactionBody.wrappedValue(connection) {
