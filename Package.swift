be4ca134bcb9ee0e6a7094a086ecb8c6658be1ef
Susan Cheng <susan.doggie@gmail.com>
2022-08-22 22:19:20 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
         .library(name: "DBVapor", targets: ["DBVapor"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-atomics", exact: "1.0.2"),
+        .package(url: "https://github.com/apple/swift-atomics", from: "1.0.2"),
         .package(url: "https://github.com/apple/swift-async-algorithms", exact: "0.0.1"),
         .package(url: "https://github.com/SusanDoggie/Doggie.git", from: "6.6.34"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.39.0"),
